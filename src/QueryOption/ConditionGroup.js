cdc4f2bf859751f0cefb514ed4a746b1610ee290
Gabriel SULLICE <gabriel@sullice.com>
2016-05-05 14:58:49 -0600

    Add ConditionGroup type

--- src/QueryOption/ConditionGroup.js
+++ src/QueryOption/ConditionGroup.js
@@ -1,5 +1,40 @@
-var ConditionGroup = function (conjunction) {
+var Condition = require("./Condition.js");
+
+var ConditionGroup = function (conjunction, group) {
   this.conjunction = conjunction ? conjunction : "AND";
+  if (group) this.group = group;
+
+  this.children = [];
+};
+
+ConditionGroup.prototype.attach = function (id, query) {
+  var parameters = [];
+
+  parameters.push({ name: "conjunction", value: this.conjunction });
+
+  if (this.group) {
+    parameters.push({ name: "group", value: this.group });
+  }
+
+  Array.prototype.push.apply(query, parameters.map(function (parameter) {
+    parameter.key = "group_" + id;
+    return parameter;
+  }));
+};
+
+ConditionGroup.prototype.condition = function (field, value, operator, langcode) {
+  this.children.push(new Condition(field, value, operator, langcode));
+  return this;
+};
+
+ConditionGroup.prototype.andConditionGroup = function () {
+  this.children.push(new ConditionGroup("AND"));
+  return this;
+};
+
+ConditionGroup.prototype.orConditionGroup = function () {
+  this.children.push(new ConditionGroup("OR"));
+  return this;
 };
 
 module.exports = ConditionGroup;
---
+++ tests/ConditionGroup.js
@@ -0,0 +1,85 @@
+var assert = require('chai').assert;
+
+var EntityQuery = require('../dist/EntityQuery');
+var ConditionGroup = require('../dist/QueryOption/ConditionGroup');
+var Condition = require('../dist/QueryOption/Condition');
+
+describe('ConditionGroup', function () {
+
+  describe('#attach()', function () {
+    it('should correctly create parameters.', function () {
+      var variations = [
+        { conjunction: "AND", group: "group_0" },
+        { conjunction: "OR", group: "group_0" },
+        { conjunction: "AND" },
+        { conjunction: "OR" },
+        { group: "group_0" },
+        { },
+      ];
+
+      var assert_variation = function (variation) {
+        var query, group, expected;
+
+        query = [];
+        group = new ConditionGroup(variation.conjunction, variation.group);
+        group.attach(1, query);
+
+        expected = [];
+
+        if (variation.conjunction) {
+          expected.push({ name: "conjunction", value: variation.conjunction, key: "group_1" });
+        } else {
+          expected.push({ name: "conjunction", value: "AND", key: "group_1" });
+        }
+
+        if (variation.group) {
+          expected.push({ name: "group", value: variation.group, key: "group_1" });
+        }
+
+        assert.deepEqual(query, expected);
+      };
+
+      variations.forEach(assert_variation);
+    });
+  });
+
+  describe('#condition', function () {
+    it('should accept conditions', function () {
+      var group;
+
+      group = new ConditionGroup();
+      group.condition("field0", "value0");
+
+      assert.deepEqual(group.children, [
+        new Condition("field0", "value0"),
+      ]);
+    });
+  });
+
+  describe('#andConditionGroup', function () {
+    it('should create child andConditionGroups', function () {
+      var group;
+
+      group = new ConditionGroup();
+      group.andConditionGroup();
+
+      assert.deepEqual(group.children, [
+        new ConditionGroup("AND"),
+      ]);
+    });
+  });
+
+  describe('#orConditionGroup', function () {
+    it('should create child orConditionGroups', function () {
+      var group;
+
+      group = new ConditionGroup();
+      group.orConditionGroup();
+
+      assert.deepEqual(group.children, [
+        new ConditionGroup("OR"),
+      ]);
+    });
+  });
+
+});
