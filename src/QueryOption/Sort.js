2cbaa1b3706a1562767c504964233c794a1f602f
Gabriel SULLICE <gabriel@sullice.com>
2016-05-05 13:25:19 -0600

    Add Sort type

--- src/QueryOption/Sort.js
+++ src/QueryOption/Sort.js
@@ -4,4 +4,20 @@ var Sort = function (field, direction, langcode) {
   this.langcode = langcode ? langcode : null;
 };
 
+Sort.prototype.attach = function (id, query) {
+  var parameters = [];
+
+  parameters.push({ name: "field", value: this.field });
+  parameters.push({ name: "direction", value: this.direction });
+
+  if (this.langcode) {
+    parameters.push({ name: "langcode", value: this.langcode });
+  }
+
+  Array.prototype.push.apply(query, parameters.map(function (parameter) {
+    parameter.key = "sort_" + id;
+    return parameter;
+  }));
+};
+
 module.exports = Sort;
---
+++ tests/Sort.js
@@ -0,0 +1,64 @@
+var assert = require('chai').assert;
+
+var EntityQuery = require('../dist/EntityQuery');
+var Sort = require('../dist/QueryOption/Sort');
+
+describe('Sort', function () {
+
+  describe('#attach()', function () {
+    it('should correctly create parameters.', function () {
+      var variations = [
+        { field: "field0", direction: "ASC", langcode: "en-US" },
+        { field: "field0", direction: "DESC", langcode: "en-US" },
+        { field: "field0", direction: "ASC" },
+        { field: "field0", direction: "DESC" },
+        { field: "field0", langcode: "en-US" },
+        { field: "field0" },
+      ];
+
+      var assert_variation = function (variation) {
+        var query, sort, expected;
+
+        query = [];
+        sort = new Sort(
+          variation.field,
+          variation.direction,
+          variation.langcode
+        )
+        sort.attach(0, query);
+
+        expected = [
+          { name: "field", value: variation.field, key: "sort_0" },
+        ];
+
+        if (variation.direction) {
+          expected.push({
+            name: "direction",
+            value: variation.direction,
+            key: "sort_0"
+          })
+        } else {
+          expected.push({
+            name: "direction",
+            value: "ASC",
+            key: "sort_0"
+          })
+        }
+
+        if (variation.langcode) expected.push({
+          name: "langcode",
+          value: variation.langcode,
+          key: "sort_0"
+        });
+
+        assert.deepEqual(
+          query, expected
+        );
+
+      };
+
+      variations.forEach(assert_variation);
+    });
+  });
+
+});
