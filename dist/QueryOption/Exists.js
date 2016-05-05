e55000c12de016347f62f4945d895502c7825a3c
Gabriel SULLICE <gabriel@sullice.com>
2016-05-04 18:42:59 -0600

    Work on implementing Exists.

--- dist/QueryOption/Exists.js
+++ dist/QueryOption/Exists.js
@@ -4,4 +4,21 @@ var Exists = function (field, langcode, condition) {
   this.condition = (typeof condition === undefined) ? true : condition;
 };
 
+Exists.prototype.attach = function (id, query) {
+  var parameters = [];
+  var conditionValue = this.condition ? "TRUE" : "FALSE";
+
+  parameters.push({ name: "field", value: this.field });
+  parameters.push({ name: "condition", value: conditionValue });
+
+  if (this.langcode) {
+    parameters.push({ name: "langcode", value: this.langcode });
+  }
+
+  Array.prototype.push.apply(query, parameters.map(function (parameter) {
+    parameter.key = "exists_" + id;
+    return parameter;
+  }));
+};
+
 module.exports = Exists;
---
+++ tests/Exists.js
@@ -0,0 +1,28 @@
+var assert = require('chai').assert;
+
+var EntityQuery = require('../dist/EntityQuery');
+var Exists = require('../dist/QueryOption/Exists');
+
+describe('Exists', function () {
+
+  describe('#attach()', function () {
+    it('should append all exists query parameters to a query.', function () {
+      var exists = new Exists('title', "en-US", true);
+      var query = [];
+      exists.attach(0, query);
+      assert.deepEqual(query[0], { name: "field", value: "title", key: "exists_0" });
+      assert.deepEqual(query[1], { name: "condition", value: "TRUE", key: "exists_0" });
+      assert.deepEqual(query[2], { name: "langcode", value: "en-US", key: "exists_0" });
+    });
+
+    it('should append all exists query parameters to a query without a default langcode.', function () {
+      var exists = new Exists('title', null, true);
+      var query = [];
+      exists.attach(0, query);
+      query.forEach(function (parameter) {
+        assert.propertyNotVal(parameter, "name", "langcode");
+      });
+    });
+  });
+
+});
