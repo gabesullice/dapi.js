2f2ca84947b9184198a06230a2d43aa6fa3ea333
Gabriel SULLICE <gabriel@sullice.com>
2016-05-06 09:40:42 -0600

    Add regression test for Exists initialization

--- tests/Exists.js
+++ tests/Exists.js
@@ -1,13 +1,13 @@
-var assert = require('chai').assert;
+var assert = require("chai").assert;
 
-var EntityQuery = require('../dist/EntityQuery');
-var Exists = require('../dist/QueryOption/Exists');
+var EntityQuery = require("../dist/EntityQuery");
+var Exists = require("../dist/QueryOption/Exists");
 
-describe('Exists', function () {
+describe("Exists", function () {
 
-  describe('#attach()', function () {
-    it('should append all exists query parameters to a query.', function () {
-      var exists = new Exists('title', "en-US", true);
+  describe("#attach()", function () {
+    it("should append all exists query parameters to a query.", function () {
+      var exists = new Exists("title", "en-US", true);
       var query = [];
       exists.attach(0, query);
       assert.deepEqual(query[0], { name: "field", value: "title", key: "exists_0" });
@@ -15,13 +15,28 @@ describe('Exists', function () {
       assert.deepEqual(query[2], { name: "langcode", value: "en-US", key: "exists_0" });
     });
 
-    it('should append all exists query parameters to a query without a default langcode.', function () {
-      var exists = new Exists('title', null, true);
+    it("should append all exists query parameters to a query without a default langcode.", function () {
+      var exists = new Exists("title", null, true);
       var query = [];
       exists.attach(0, query);
       query.forEach(function (parameter) {
         assert.propertyNotVal(parameter, "name", "langcode");
       });
+    });
+
+    it("should propertly default to exists instead of notExists.", function () {
+      var exists = new Exists("title");
+      assert.isTrue(exists.condition);
+    });
+
+    it("should propertly initialize a notExists.", function () {
+      var exists;
+
+      exists = new Exists("title", null, false);
+      assert.isFalse(exists.condition);
+
+      exists = new Exists("title", "en-us", false);
+      assert.isFalse(exists.condition);
     });
   });
 
